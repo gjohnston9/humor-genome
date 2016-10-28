6e8c728b63db89b8e5bf8f58df56c0849fe6eeaa
greg9381 <greg.johnston@gatech.edu>
2016-10-28 10:55:44 -0400

    fix typo in creation of jokes_collection; update connection parameters

--- text_analytics/main.py
+++ text_analytics/main.py
@@ -2,11 +2,13 @@ from joke_collection import JokeCollection
 
 import pymongo
 
-with pymongo.MongoClient("localhost", 27017) as client:
-	db = client.hgp_jokerz
-	collection = db.jokes
+connection_string = "mongodb://localhost:27018"
 
-	jokes = (joke for joke in collection.find().limit(1000))
-	jokes_collection = JokeCollection(jokes_list)
+with pymongo.MongoClient(connection_string) as client:
+	db = client.hgp_jokerz
+	collection = db.JokesCleaned
+
+	jokes = collection.find().limit(1000)
+	jokes_collection = JokeCollection(jokes)
 	# jokes_collection.write_jokes("first_1000_jokes")
 	print jokes_collection.max_tf_idf_by_category(n=10)
