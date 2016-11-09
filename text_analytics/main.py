807d9457d40de4d402e1ec5e8cf2f5ea990acd61
greg9381 <greg.johnston@gatech.edu>
2016-11-09 10:23:12 -0500

    update main.py to python 3

--- text_analytics/main.py
+++ text_analytics/main.py
@@ -17,5 +17,5 @@ with pymongo.MongoClient(connection_string) as client:
 	jokes = collection.find().limit(num_jokes)
 	jokes_collection = JokeCollection(jokes)
 	# for category, terms in jokes_collection.max_tf_idf_by_category(n=top_n_terms, debug=True).items():
-	# 	print "{}: {}".format(category, terms)
+	# 	print("{}: {}".format(category, terms))
 	jokes_collection.test_classifier(nltk.NaiveBayesClassifier, jokes_collection.BOW_feature_extractor, debug=True)
