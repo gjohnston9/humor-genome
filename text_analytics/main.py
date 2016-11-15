00bfa21edc10a784973b563c1cb127b69a04e0b0
Anna Herrera <aherrera8@gatech.edu>
2016-11-15 14:59:29 -0500

    Example Automatic Classification

--- text_analytics/main.py
+++ text_analytics/main.py
@@ -12,7 +12,7 @@ import contextlib
 parser = argparse.ArgumentParser()
 parser.add_argument("--verbose", help="increase output verbosity", action="store_true")
 parser.add_argument("--ssh", help="while script is running, establish ssh connection, using the "
-	"parameters stored in ssh.txt", action="store_true")
+    "parameters stored in ssh.txt", action="store_true")
 parser.add_argument("--port", help="connect to your Mongo database through the provided local port", type=int, required=True)
 args = parser.parse_args()
 
@@ -23,30 +23,49 @@ top_n_terms = 10
 
 
 with contextlib.ExitStack() as stack: # gives the ability to use conditional context managers
-	if args.ssh:
-		# get ssh connection parameters
-		with open("ssh.txt", "r") as f:
-			host = f.readline().strip()
-			user = f.readline().strip()
-			pwd = f.readline().strip()
-		if args.verbose: print("opening ssh connection to {}".format(host))
-		# establish ssh connection
-		ssh_conn = stack.enter_context(SSHTunnelForwarder(
-			host,
-			ssh_username=user,
-			ssh_password=pwd,
-			local_bind_address=("0.0.0.0", args.port),
-			remote_bind_address=("127.0.0.1", 27017)))
-		if args.verbose: print("opened ssh connection")
+    if args.ssh:
+        # get ssh connection parameters
+        with open("ssh.txt", "r") as f:
+            host = f.readline().strip()
+            user = f.readline().strip()
+            pwd = f.readline().strip()
+        if args.verbose: print("opening ssh connection to {}".format(host))
+        # establish ssh connection
+        ssh_conn = stack.enter_context(SSHTunnelForwarder(
+            host,
+            ssh_username=user,
+            ssh_password=pwd,
+            local_bind_address=("0.0.0.0", args.port),
+            remote_bind_address=("127.0.0.1", 27017)))
+        if args.verbose: print("opened ssh connection")
 
-	# establish mongo connection
-	client = stack.enter_context(pymongo.MongoClient(connection_string))
-	print("connected to {}".format(connection_string))
-	db = client.hgp_jokerz
-	collection = db.JokesCleaned
+    # establish mongo connection
+    client = stack.enter_context(pymongo.MongoClient(connection_string))
+    print("connected to {}".format(connection_string))
+    db = client.hgp_jokerz
+    collection = db.JokesCleaned
+    keywords = {}
+    jokes = collection.find().limit(num_jokes)
+    jokes_collection = JokeCollection(jokes)
+    for category, terms in jokes_collection.max_tf_idf_by_category(n=top_n_terms, debug=args.verbose).items():
+       print("{}: {}".format(category, terms))
+       keywords[category] = terms
 
-	jokes = collection.find().limit(num_jokes)
-	jokes_collection = JokeCollection(jokes)
-	# for category, terms in jokes_collection.max_tf_idf_by_category(n=top_n_terms, debug=args.verbose).items():
-	# 	print("{}: {}".format(category, terms))
-	jokes_collection.test_classifier(nltk.NaiveBayesClassifier, jokes_collection.BOW_feature_extractor, debug=args.verbose)
+    #Auto Classification Demo
+    jokes = collection.find().limit(num_jokes)
+    categories = keywords.keys()
+    i = 1
+    while i <= 5:
+        print("\nExample " + str(i))
+        singlejoke = jokes.next()
+        print("Joke\n" + JokeCollection.remove_punctuation(singlejoke["content"]))
+        print("Existing Categories: " + singlejoke["categories"])
+        catSet = set()
+        for cat in categories:
+            for keyword in keywords[cat]:
+                if keyword in singlejoke["content"]:
+                    catSet.add(cat);
+        i = i + 1
+        print("Automatically classify: " + str(catSet))
+
+    #jokes_collection.test_classifier(nltk.NaiveBayesClassifier, jokes_collection.BOW_feature_extractor, debug=args.verbose)
